3669cc94702a80eff848c0d32e60c39014a6a6df
lorxx504-Kongmeng <lorxx504@umn.edu>
2022-12-08 23:55:03 -0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccountComponent } from './account/account.component';
+import { MainComponent } from './main/main.component';
+import {MatCardModule} from '@angular/material/card';
+import {HttpClientModule} from "@angular/common/http";
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    MatToolbarModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    NgbModule,
+    MatCardModule,
+    HttpClientModule,
+    MatInputModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
